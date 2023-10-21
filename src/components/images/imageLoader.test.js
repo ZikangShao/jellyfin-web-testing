d436c21e54553b9b425ae14c2acc1492402ce353
ZikangShao <shaozikang99@gmail.com>
2023-10-20 21:11:23 -0700

    added more tests

--- src/components/images/imageLoader.test.js
+++ src/components/images/imageLoader.test.js
@@ -2,17 +2,52 @@ import { jest } from '@jest/globals';
 
 const getPrimaryImageAspectRatio = require('./imageLoader.js');
 
+/*
 class aspectRatio {
     constructor(ratio) {
         this.PrimaryImageAspectRatio = ratio;
     }
-}
+}*/
 
 jest.test('Given zero items, get null returned', () => {
-    jest.expect(getPrimaryImageAspectRatio([])).toBe(null);
+    const items = [];
+
+    const result = getPrimaryImageAspectRatio(items);
+    jest.expect(result).toBeNull();
 });
 
-jest.test('Given 3:4 aspect ratio, returns 2:3', () => {
-    jest.expect(getPrimaryImageAspectRatio([aspectRatio(3 / 4)])).toBe(2 / 3);
+jest.test('Given 201:300 aspect ratio, returns 2:3', () => {
+    const items = [
+        { PrimaryImageAspectRatio: 201 / 300 }
+    ];
+
+    const result = getPrimaryImageAspectRatio(items);
+    jest.expect(result).toBeCloseTo(2 / 3);
 });
 
+jest.test('Given 32:19 aspect ratio, returns 16:9', () => {
+    const items = [
+        { PrimaryImageAspectRatio: 32 / 19 }
+    ];
+
+    const result = getPrimaryImageAspectRatio(items);
+    jest.expect(result).toBeCloseTo(16 / 9);
+});
+
+jest.test('Given 100:99 aspect ratio, returns 1', () => {
+    const items = [
+        { PrimaryImageAspectRatio: 100 / 99 }
+    ];
+
+    const result = getPrimaryImageAspectRatio(items);
+    jest.expect(result).toBeCloseTo(1);
+});
+
+jest.test('Given 401/300 aspect ratio, returns 2:3', () => {
+    const items = [
+        { PrimaryImageAspectRatio: 401 / 300 }
+    ];
+
+    const result = getPrimaryImageAspectRatio(items);
+    jest.expect(result).toBeCloseTo(2 / 3);
+});
