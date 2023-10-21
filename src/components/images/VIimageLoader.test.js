53157636822c870d5118475a32e48edc10508812
ZikangShao <shaozikang99@gmail.com>
2023-10-20 21:50:13 -0700

    vitest attempt

---
+++ src/components/images/VIimageLoader.test.js
@@ -0,0 +1,47 @@
+import { describe, expect, test } from 'vitest';
+import imageLoader from './imageLoader';
+
+describe('getPrimaryImageAspectRatio', () => {
+    test('Given zero items, get null returned', () => {
+        const items = [];
+
+        const result = imageLoader.getPrimaryImageAspectRatio(items);
+        expect(result).toBeNull();
+    });
+
+    test('Given 201:300 aspect ratio, returns 2:3', () => {
+        const items = [
+            { PrimaryImageAspectRatio: 201 / 300 }
+        ];
+
+        const result = imageLoader.getPrimaryImageAspectRatio(items);
+        expect(result).toBeCloseTo(2 / 3);
+    });
+
+    test('Given 32:19 aspect ratio, returns 16:9', () => {
+        const items = [
+            { PrimaryImageAspectRatio: 32 / 19 }
+        ];
+
+        const result = imageLoader.getPrimaryImageAspectRatio(items);
+        expect(result).toBeCloseTo(16 / 9);
+    });
+
+    test('Given 100:99 aspect ratio, returns 1', () => {
+        const items = [
+            { PrimaryImageAspectRatio: 100 / 99 }
+        ];
+
+        const result = imageLoader.getPrimaryImageAspectRatio(items);
+        expect(result).toBeCloseTo(1);
+    });
+
+    test('Given 401/300 aspect ratio, returns 2:3', () => {
+        const items = [
+            { PrimaryImageAspectRatio: 401 / 300 }
+        ];
+
+        const result = imageLoader.getPrimaryImageAspectRatio(items);
+        expect(result).toBeCloseTo(2 / 3);
+    });
+});
