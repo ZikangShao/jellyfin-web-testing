896e35ead3ead4da65946e2ec9ca18597cf144ee
Emily <emilysoolee@gmail.com>
2023-11-27 00:15:25 +0000

    added exlusion criteria for tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     test: {
+        include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        exclude: [
+            '**/node_modules/**',
+            '**/dist/**',
+            '**/.{idea,git,cache,output,temp}/**',
+            './src/config/**',
+            './UiTest/**/*.{ts,js}'
+        ],
         watch: false,
         environment: 'jsdom',
         coverage: {
