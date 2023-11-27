b7b611691cb482d8db22041139b2da0eb2fcb964
ZikangShao <shaozikang99@gmail.com>
2023-11-26 16:02:31 -0800

    ui test in regression

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -1,12 +1,12 @@
-# #!/bin/bash 
+# #!/bin/bash
 
-# To make builTestSuite.sh an executable file, run chmod +x buildTestSuite.sh 
+# To make builTestSuite.sh an executable file, run chmod +x buildTestSuite.sh
 # and ./buildTestSuite seattleu.team1@gmail.com to run script
 
 email=$1
 PROJECT_PATH=".\jellyfin-web-testing"
 
-# Build Software Under Test 
+# Build Software Under Test
 echo "Building Software Under Test..."
 cd $PROJECT_PATH
 npm run build:development
@@ -14,20 +14,30 @@ npm run build:development
 # Check building software under test was successful
 if [ $? -eq 0 ]; then
     BUILD_SUCCESS='true'
-else 
+else
     BUILD_SUCCESS='false'
 fi
 
-# Run Test Code via script 
+# Run Test Code via script
 echo "Running test code..."
 npm run test:coverage
 
-# check whether all tests passed 
+# check whether all tests passed
 if [ $? -eq 0 ]; then
     TEST_PASS='true'
-else 
+else
     TEST_PASS='false'
 fi
+
+# Run UI test via script
+#npm run test:UI
+
+# check whether all UI tests passed
+#if [ $? -eq 0 ]; then
+#    UI_TEST_PASS='true'
+#else
+#    UI_TEST_PASS='false'
+#fi
 
 # email the status of build and tests to the specified email address using curl
 echo "Sending email to $email..."
@@ -40,7 +50,7 @@ curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
 
 if [ $? -eq 0 ]; then
     echo 'Status email sent!'
-else 
+else
     echo 'Something went wrong... email was not delivered'
 fi
 
